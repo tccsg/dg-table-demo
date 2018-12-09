ded17c880a300554cd8cfefa8a20dac010f72943
tccsg <547325725@qq.com>
2018-12-09 22:10:17 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloDGTable/>
+  </div>
+</template>
+
+<script>
+import HelloDGTable from './components/HelloDGTable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloDGTable
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import DGTable from 'dg-table'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+Vue.use(DGTable)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
