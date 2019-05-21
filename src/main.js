602441f39abac743fafb4a730bdbfbbad51ce33a
tccsg <chenshaogan@chenshaogandeMacBook.local>
2019-05-21 21:58:00 +0800

    :bug: 兼容ie

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import DGTable from 'dg-table'
+import '@babel/polyfill'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.use(DGTable)
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   // 基本路径
   baseUrl: './',
   // 生产环境是否生成 sourceMap 文件
-  productionSourceMap: false
+  productionSourceMap: false,
+  transpileDependencies: []
 }
